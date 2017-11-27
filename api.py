5974dbc7bae845b01f72aa6592a9cb471baee812
rodrigomelo01@gmail.co <rodrigomelo01@gmail.com>
2017-11-26 22:09:45 -0200

    Conclusão do CRUD do Pesquisador

--- api.py
+++ api.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api, reqparse, abort
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 api = Api(app)
-
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -17,11 +16,86 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql = MySQL()
 mysql.init_app(app)
 
-class CreatePesquisador(Resource):
+# Cria a conexao com o banco de dados.
+conn = mysql.connect()
+
+# Cria um cursor para iterar sobre os elementos retornados do banco.
+cursor = conn.cursor()
+
+# Cria um parser para tipar os parâmetros de um request.
+parser = reqparse.RequestParser()
+
+
+class Pesquisador(Resource):
+    def get(self, id):
+        try:
+            # Busca todos os pesquisadores cadastrados
+            query = "SELECT * FROM universitat.pesquisador where id = %s"
+
+            cursor.execute(query, (id))
+
+            pesquisador = [dict((cursor.description[i][0], value)
+                                for i, value in enumerate(row)) for row in cursor.fetchmany(1)]
+
+            if pesquisador is None:
+                return "Pesquisador não encontrado", 404
+            else:
+                return pesquisador
+
+        except Exception as e:
+            return {'error': str(e)}
+
+    def delete(self, id):
+        try:
+            # Busca todos os pesquisadores cadastrados
+            query = "DELETE FROM universitat.pesquisador where id = %s"
+
+            cursor.execute(query, (id))
+
+            conn.commit()
+
+        except Exception as e:
+            return {'error': str(e)}
+
+    def put(self, id):
+        try:
+            parser.add_argument('nome', type=str, help='Nome do pesquisador.')
+            parser.add_argument('cpf', type=int, help='CPF do pesquisador.')
+            args = parser.parse_args()
+
+            _nome = args['nome']
+            _cpf = args['cpf']
+
+            # Busca todos os pesquisadores cadastrados
+            query = "UPDATE `universitat`.`pesquisador` SET `nome` = %s, `cpf` = %s WHERE `id` = %s"
+
+            cursor.execute(query, (_nome, _cpf, id))
+
+            conn.commit()
+
+        except Exception as e:
+            return {'error': str(e)}
+
+
+class PesquisadorList(Resource):
+    def get(self):
+        try:
+            # Busca todos os pesquisadores cadastrados
+            query = "SELECT * FROM universitat.pesquisador"
+
+            cursor.execute(query)
+
+            pesquisadores = [dict((cursor.description[i][0], value)
+                                  for i, value in enumerate(row)) for row in cursor.fetchall()]
+
+            return pesquisadores, 200
+
+        except Exception as e:
+            return {'error': str(e)}
+
     def post(self):
         try:
             # Parse the arguments
-            parser = reqparse.RequestParser()
             parser.add_argument('id', type=int, help='ID do pesquisador.')
             parser.add_argument('nome', type=str, help='Nome do pesquisador.')
             parser.add_argument('cpf', type=int, help='CPF do pesquisador.')
@@ -37,7 +111,7 @@ class CreatePesquisador(Resource):
             # Cria um cursor para iterar sobre os elementos retornados do banco.
             cursor = conn.cursor()
 
-            query = "INSERT INTO `universitat`.`pesquisador` (`id`,`nome`,`cpf`) VALUES (%s,%s,%s);"
+            query = "INSERT INTO `universitat`.`pesquisador` (`id`,`nome`,`cpf`) VALUES (%s,%s,%s)"
 
             cursor.execute(query, (_id, _nome, _cpf))
 
@@ -49,7 +123,8 @@ class CreatePesquisador(Resource):
             return {'error': str(e)}
 
 
-api.add_resource(CreatePesquisador, '/CreatePesquisador')
+api.add_resource(Pesquisador, '/pesquisadores/<id>')
+api.add_resource(PesquisadorList, '/pesquisadores')
 
 if __name__ == '__main__':
     app.run(debug=True)
